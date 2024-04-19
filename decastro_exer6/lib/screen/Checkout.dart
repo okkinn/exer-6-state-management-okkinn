32d0ca42ae4d02fd9de1dc0da0d750326a55a21a
Nico De Castro <jtdecastro@up.edu.ph>
2024-04-20 02:04:07 +0800

    feat: add sample state management app and add checkout page

---
+++ decastro_exer6/lib/screen/Checkout.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import '../model/Item.dart';
+import "package:provider/provider.dart";
+import "../provider/shoppingcart_provider.dart";
+
+class Checkout extends StatelessWidget {
+  const Checkout({super.key});
+
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: const Text("Checkout")),
+        body: Column(
+          children: [
+            const Text("Item Details"),
+            Divider(height: 1, color: Colors.grey[300]),
+            getCheckoutItems(context),
+            Divider(height: 1, color: Colors.grey[300]),
+              computeCost(),
+              Flexible(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          context.read<ShoppingCart>().removeAll();
+                          Navigator.pop(context, "Payment Successful!");
+                        },
+                        child: const Text("Pay Now!")),
+                    ],
+                ),
+              ),
+            ]
+        ),
+      );
+    }
+
+    Widget getCheckoutItems(BuildContext context) {
+      List<Item> products = context.watch<ShoppingCart>().cart;
+      String productname = "";
+      return Expanded(
+                child: Column(
+                children: [
+                  Flexible(
+                      child: ListView.builder(
+                      itemCount: products.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(products[index].name),
+                          trailing: Text(products[index].price.toStringAsFixed(2)),
+                      );
+                    },
+                  )),
+                  ],
+                ));
+  }
+}
+
+Widget computeCost() {
+    return Consumer<ShoppingCart>(builder: (context, cart, child) {
+       return Text("Total Cost to Pay: ${cart.cartTotal}");
+    });
+  }
